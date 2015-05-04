7dfc64d3b8726dff6855f4787a8dfcdf49eace92
Sean May <smmay2@gmail.com>
2015-05-04 11:40:27 -0400

    couple more sections done!

---
+++ c03/js/min/adding-and-removing-properties-min.js
@@ -0,0 +1,1 @@
+var hotel={name:"Quay",rooms:70,booked:54};hotel.gym=!0,hotel.pool=!1,delete hotel.booked;var elName=document.getElementById("hotelName");elName.textContent=hotel.name;var elPool=document.getElementById("pool");elPool.className="Pool: "+hotel.pool;var elGym=document.getElementById("gym");elGym.className="Gym: "+hotel.gym;
